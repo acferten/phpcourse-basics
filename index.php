ee307d95234c68afe4bbb9a2ce61db3832b1b188
acferten <gowiththeirname@gmail.com>
2022-11-06 21:42:50 +0700

    Домашнее задание

--- index.php
+++ index.php
@@ -1,19 +1,33 @@
 <?php
-if ('string') {
-    echo ' string: Условие выполнилось ';
+function minimalNumber(float $a, float $b, float $c)
+{
+    return min($a, $b, $c);
 }
 
-if (0) {
-    echo '0: Условие выполнилось ';
-}
-if (null) {
-    echo 'null: Условие выполнилось ';
-}
-if (5) {
-    echo '5: Условие выполнилось ';
+function multiTo2(&$x, &$y)
+{
+    $x *= 2;
+    $y *= 2;
 }
 
-$two = 5 % 2 == 0 ? 'Четное ' : 'Нечетное ';
-echo $two;
+function numbers($a)
+{
+    if ($a == 0) {
+        echo ' // Числа от 0 до n ' . 0;
+        return 0;
+    }
+    numbers($a - 1);
+    echo ', ' . $a;
+}
+
+$x = 13.97;
+$c = 5.5;
+$v = 8.5;
+
+
+echo 'Минимальное число: ' . minimalNumber($x, $c, $v);
+multiTo2($c, $v);
+echo ' // Умножение на 2: ' . $c . '--' . $v;
+numbers(5)
 
 ?>
