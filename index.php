bee3d5b768df6af3d69ff57f79a3520ac1cb5098
acferten <gowiththeirname@gmail.com>
2022-11-07 00:17:46 +0700

    Домашнее задание

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php
 
-$n = strtoupper('oneoneone');
+function isIncluded(array $arr, int $n)
+{
+    foreach ($arr as $element) {
+        if ($element === $n) {
+            return true;
+        }
+    }
+    return false;
+}
 
-echo $n;
+function countAmount(array $arr, int $n)
+{
+    $counter = 0;
+    foreach ($arr as $element) {
+        if ($element === $n) {
+            $counter++;
+        }
+    }
+    return $counter;
+}
 
-echo "\n" . strrev($n);
+$n = 5;
+$A = [1, 5, 5, 5, 7, 8, 5, 9, 0];
+
+echo "-- элемент $n в массиве    " . var_dump(isIncluded($A, $n));
+echo "-- количество элемента $n в массиве" . var_dump(countAmount($A, $n));
+
