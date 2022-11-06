729d60618561ca3d83505471ab6547ba72242744
acferten <gowiththeirname@gmail.com>
2022-11-06 19:14:18 +0700

    Домашнее задание Попробуйте следующие условия

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
-echo '=== !1,  ' . var_dump(!1);
-echo '=== !0,  ' . var_dump(!0);
-echo '=== !true,  ' . var_dump(!true);
-echo '=== 2 && 3,  ' . var_dump(2 && 3);
-echo '=== 5 && 0,  ' . var_dump(5 && 0);
-echo '=== 3 || 0,  ' . var_dump(3 || 0);
-echo '=== 5 / 1,  ' . var_dump(5 / 1);
-echo '=== 1 / 5,  ' . var_dump(1 / 5);
-echo "=== 5 + '5string',  " . var_dump(5 + '5string');
-echo "=== '5'==5,  " . var_dump('5' == 5);
-echo "=== '05' == 5,  " . var_dump('05' == 5);
-echo "=== '05' == '5',  " . var_dump('05' == '5');
+if ('string') {
+    echo ' string: Условие выполнилось ';
+}
+
+if (0) {
+    echo '0: Условие выполнилось ';
+}
+if (null) {
+    echo 'null: Условие выполнилось ';
+}
+if (5) {
+    echo '5: Условие выполнилось ';
+}
+
+$two = 5 % 2 == 0 ? 'Четное ' : 'Нечетное ';
+echo $two;
+
+?>
