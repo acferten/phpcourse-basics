45d8530b7be00d73db2bb87df9aa2670c19319b7
acferten <gowiththeirname@gmail.com>
2022-11-07 09:30:31 +0700

    Домашнее задание

--- index.php
+++ index.php
@@ -1,23 +1,18 @@
-<?php $a = 3;
-$b = 5;
-$c = 0;
-echo "| a: $a\n";
-echo "b: $b\n";
-$c = $a;
-$a = $b;
-$b = $c;
-echo "| a: $a\n";
-echo "b: $b | ";
-?>
-
-<?php
-$a = 3;
-$b = 5;
-echo "// | a: $a\n";
-echo "b: $b\n";
-$a = $a + $b;
-$b = $a - $b;
-$a = $a - $b;
-echo "| a: $a\n";
-echo "b: $b\n |";
-?>
+<html>
+<head>
+    <title>Форма входа</title>
+</head>
+<body>
+<form action="/login.php" method="get">
+    <label>
+        Логин <input type="text" name="login">
+    </label>
+    <br>
+    <label>
+        Пароль <input type="password" name="password">
+    </label>
+    <br>
+    <input type="submit" value="Войти">
+</form>
+</body>
+</html>
