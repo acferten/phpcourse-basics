0baf551d0e5c3f64b1e136a2f76627e3072a5870
acferten <gowiththeirname@gmail.com>
2022-11-08 03:03:48 +0700

    Домашнее задание

--- index.php
+++ index.php
@@ -1,18 +1,9 @@
 <?php
-require __DIR__ . '/auth.php';
-$login = getUserLogin();
-?>
-<html>
-<head>
-    <title>Главная страница</title>
-</head>
-<body>
-<?php if ($login === null): ?>
-    <a href="/login.php">Авторизуйтесь</a>
-<?php else: ?>
-    Добро пожаловать, <?= $login ?>
-    <br>
-    <a href="/logout.php">Выйти</a>
-<?php endif; ?>
-</body>
-</html>
+echo htmlentities(file_get_contents(__FILE__));
+
+$files = scandir(__DIR__ . '/');
+foreach ($files as $file) {
+    if (is_dir($file)) {
+        echo $file . '<br>';
+    }
+}
--- logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-setcookie('login', '', 0, '/');
-setcookie('password', '', 0, '/');
-header('Location: /index.php');
