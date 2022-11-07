7674fc3bd23b2b67cbc2d88e6c948391a70eab0e
acferten <gowiththeirname@gmail.com>
2022-11-08 00:34:50 +0700

    Домашнее задание

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
-<?php $a = 3;
-$b = 5;
-$c = 0;
-echo "| a: $a\n";
-echo "b: $b\n";
-$c = $a;
-$a = $b;
-$b = $c;
-echo "| a: $a\n";
-echo "b: $b | ";
-?>
-
 <?php
-$a = 3;
-$b = 5;
-echo "// | a: $a\n";
-echo "b: $b\n";
-$a = $a + $b;
-$b = $a - $b;
-$a = $a - $b;
-echo "| a: $a\n";
-echo "b: $b\n |";
+setcookie('login', 'admin', time()+20, '/');
+echo 'Cookie установлены';
+print_r($_COOKIE);
 ?>
