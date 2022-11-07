d0e5b0d03bda5497146ee1c5dfcad9e564d4971e
acferten <gowiththeirname@gmail.com>
2022-11-08 02:34:03 +0700

    23

--- index.php
+++ index.php
@@ -340,4 +340,24 @@ echo 'Cookie установлены';
 print_r($_COOKIE);
 ?>
 
+<!--Система авторизации с помощью cookie на PHP-->
+
+<?php
+require __DIR__ . '/auth.php';
+$login = getUserLogin();
+?>
+<html>
+<head>
+    <title>Главная страница</title>
+</head>
+<body>
+<?php if ($login === null): ?>
+    <a href="/login.php">Авторизуйтесь</a>
+<?php else: ?>
+    Добро пожаловать, <?= $login ?>
+    <br>
+    <a href="/logout.php">Выйти</a>
+<?php endif; ?>
+</body>
+</html>
 
--- login.php
+++ login.php
@@ -1,3 +1,5 @@
+<!--какой-то прошлый урок-->
+
 <?php
 $login = !empty($_GET['login']) ? $_GET['login'] : '';
 $password = !empty($_GET['password']) ? $_GET['password'] : '';
@@ -23,3 +25,55 @@ if ($login != 'admin') {
 </p>
 </body>
 </html>
+
+<!--Авторизация с куки 22-->
+
+<?php
+require __DIR__ . '/auth.php';
+
+
+if (!empty($_COOKIE)) {
+    $login = $_COOKIE['login'] ?? '';;
+    $password = $_COOKIE['password'] ?? '';;
+    if (checkAuth($login, $password)) {
+        header('Location: /index.php');
+    }
+}
+
+if (!empty($_POST)) {
+    require __DIR__ . '/auth.php';
+
+
+    $login = $_POST['login'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+    if (checkAuth($login, $password)) {
+        setcookie('login', $login, 0, '/');
+        setcookie('password', $password, 0, '/');
+        header('Location: /index.php');
+    } else {
+        $error = 'Ошибка авторизации';
+    }
+}
+?>
+<html>
+<head>
+    <title>Форма авторизации</title>
+</head>
+<body>
+<?php if (isset($error)): ?>
+    <span style="color: red;">
+    <?= $error ?>
+</span>
+<?php endif; ?>
+<form action="/login.php" method="post">
+    <label for="login">Имя пользователя: </label><input type="text" name="login" id="login">
+    <br>
+    <label for="password">Пароль: </label><input type="password" name="password" id="password">
+    <br>
+    <input type="submit" value="Войти">
+</form>
+</body>
+</html>
+
+
